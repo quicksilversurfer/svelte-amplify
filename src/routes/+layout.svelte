fc998e3ca387379b3a19f84905140f4540590749
Prateek Solanki <manu2prateek@gmail.com>
2025-01-19 11:21:44 -0800

    update amplify.yml for Node.js version management and enhance build commands; refactor Svelte layout to use AmplifyInit component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,9 @@
 <script>
   import "../app.css";
   let { children } = $props();
-  import { Amplify } from "aws-amplify";
-  import outputs from "@/amplify_outputs.json";
-
-  Amplify.configure(outputs, { ssr: true });
+  import AmplifyInit from "$lib/components/AmplifyInit.svelte";
+  import outputs from "../../amplify_outputs.json";
 </script>
 
+<AmplifyInit {outputs} />
 {@render children()}
