7392e2dc37dbed7078eaaee5fa5a180d6ed31db0
Prateek Solanki <manu2prateek@gmail.com>
2025-01-19 10:42:52 -0800

    refactor amplify.yml build commands and update Svelte layout imports for consistency

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,10 @@
 <script>
-    import "../app.css";
-    let { children } = $props();
-    import { Amplify } from 'aws-amplify';
-    import outputs from '@/amplify_outputs.json';
+  import "../app.css";
+  let { children } = $props();
+  import { Amplify } from "aws-amplify";
+  import outputs from "@/amplify_outputs.json";
 
-    Amplify.configure(outputs, { ssr: true });
+  Amplify.configure(outputs, { ssr: true });
 </script>
 
 {@render children()}
